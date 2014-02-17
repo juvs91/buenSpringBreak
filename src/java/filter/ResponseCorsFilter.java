ba9a81a8b25552ce400e496398f570363efee986
juvs <juve.guz@gmail.com>
2014-02-16 18:03:08 -0600

    subiendo el filtro

---
+++ src/java/filter/ResponseCorsFilter.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package filter;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.ext.Provider;
+
+@Provider
+public class ResponseCorsFilter implements ContainerResponseFilter{
+
+    @Override
+    public void filter(ContainerRequestContext requestContext,
+            ContainerResponseContext responseContext) throws IOException {
+           responseContext.getHeaders()
+                .putSingle("Access-Control-Allow-Origin","*");
+           responseContext.getHeaders()
+                 .putSingle("Access-Control-Allow-Methods",
+                     "GET, POST, PUT, DELETE");
+           List<String> reqHead=requestContext.getHeaders()
+                     .get("Access-Control-Request-Headers");
+           if(null != reqHead){
+                responseContext.getHeaders()
+                   .put("Access-Control-Allow-Headers", 
+                        new ArrayList<Object>(reqHead));
+           }
+    }
+
+
+}
