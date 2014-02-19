e65e74ffc2217256667f0751c73cb8f1f7795b2d
neto <neto_g90@hotmail.com>
2014-02-19 17:37:25 -0600

    arreglado cliente para IE

--- cliente/request.js
+++ cliente/request.js
@@ -3,13 +3,14 @@
 function requestCtr () {
 	var url = 'http://localhost:8080/factoryEcomation_Services/webresources/sensorgas';
     var data = {};
+    $.support.cors = true;
 	$.ajax({    
 	    type: 'GET',
 	    url: url,
 	    async: false,
 	    jsonpCallback: 'jsonCallback',
 	    contentType: "aplication/json",
-	    //dataType: 'jsonp',   
+	    dataType: 'json',   
 		//crossDomain : true, 
 	    success: function(json) {
 		   data = json; 		
