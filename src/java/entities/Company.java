0580a7be2296976f2570dab81d7bdf755e3363c6
ccastillo <ccastillo@ccastillo-HP-Pavilion-dv4-Notebook-PC.(none)>
2014-03-09 18:11:30 -0600

    subiendo las modificacion de entidades

---
+++ src/java/entities/Company.java
@@ -0,0 +1,159 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package entities;
+
+import entities.SensorCatalog;
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.Date;
+import javax.persistence.Basic;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+
+/**
+ *
+ * @author ccastillo
+ */
+@Entity
+@Table(name = "company")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Company.findAll", query = "SELECT c FROM Company c"),
+    @NamedQuery(name = "Company.findByIdCompany", query = "SELECT c FROM Company c WHERE c.idCompany = :idCompany"),
+    @NamedQuery(name = "Company.findByCompanyName", query = "SELECT c FROM Company c WHERE c.companyName = :companyName"),
+    @NamedQuery(name = "Company.findByActive", query = "SELECT c FROM Company c WHERE c.active = :active"),
+    @NamedQuery(name = "Company.findByLastUpdateDate", query = "SELECT c FROM Company c WHERE c.lastUpdateDate = :lastUpdateDate")})
+public class Company implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id_company")
+    private Integer idCompany;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 45)
+    @Column(name = "company_name")
+    private String companyName;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "active")
+    private boolean active;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "last_update_date")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastUpdateDate;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "idCompany")
+    private Collection<CommDeviceCatalog> commDeviceCatalogCollection;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "idCompany")
+    private Collection<SensorCatalog> sensorCatalogCollection;
+
+    public Company() {
+    }
+
+    public Company(Integer idCompany) {
+        this.idCompany = idCompany;
+    }
+
+    public Company(Integer idCompany, String companyName, boolean active, Date lastUpdateDate) {
+        this.idCompany = idCompany;
+        this.companyName = companyName;
+        this.active = active;
+        this.lastUpdateDate = lastUpdateDate;
+    }
+
+    public Integer getIdCompany() {
+        return idCompany;
+    }
+
+    public void setIdCompany(Integer idCompany) {
+        this.idCompany = idCompany;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public boolean getActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public Date getLastUpdateDate() {
+        return lastUpdateDate;
+    }
+
+    public void setLastUpdateDate(Date lastUpdateDate) {
+        this.lastUpdateDate = lastUpdateDate;
+    }
+
+    @XmlTransient
+    public Collection<CommDeviceCatalog> getCommDeviceCatalogCollection() {
+        return commDeviceCatalogCollection;
+    }
+
+    public void setCommDeviceCatalogCollection(Collection<CommDeviceCatalog> commDeviceCatalogCollection) {
+        this.commDeviceCatalogCollection = commDeviceCatalogCollection;
+    }
+
+    @XmlTransient
+    public Collection<SensorCatalog> getSensorCatalogCollection() {
+        return sensorCatalogCollection;
+    }
+
+    public void setSensorCatalogCollection(Collection<SensorCatalog> sensorCatalogCollection) {
+        this.sensorCatalogCollection = sensorCatalogCollection;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (idCompany != null ? idCompany.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Company)) {
+            return false;
+        }
+        Company other = (Company) object;
+        if ((this.idCompany == null && other.idCompany != null) || (this.idCompany != null && !this.idCompany.equals(other.idCompany))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Factories.Company[ idCompany=" + idCompany + " ]";
+    }
+    
+}
