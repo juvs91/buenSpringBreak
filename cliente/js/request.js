4798e8294a6fd62daa733495941760deceb2f1ea
juvs <juve.guz@gmail.com>
2014-03-26 14:04:41 -0600

    subiendo modificaciones en como se van a agrupar los puntos sin sensor

---
+++ cliente/js/request.js
@@ -0,0 +1,53 @@
+
+//initial request get all the initial sensors with all their points
+function requestSensores() {
+	var url = 'http://localhost:8080/factoryEcomation_Services/webresources/sensors';
+    var data = {};
+    $.support.cors = true;
+	$.ajax({    
+	    type: 'GET',
+	    url: url,
+	    async: false,
+	    jsonpCallback: 'jsonCallback',
+	    contentType: "aplication/json",
+	    dataType: 'json',   
+		//crossDomain : true, 
+	    success: function(json) { 
+		   data = json; 		
+	    },
+	    error: function(e) { 
+	       console.log(e);
+	    },
+	 	onload:function(json){
+			//console.log(json);
+		}
+	});
+	return data;   
+	
+}   
+
+//get the lastest points of the chart
+function requestNewPoints () {  
+	var url = "http://localhost:8080/factoryEcomation_Services/webresources/sensors";
+	var data = {};
+    $.support.cors = true;
+	$.ajax({    
+	    type: 'GET',
+	    url: url,
+	    async: true,
+	    jsonpCallback: 'jsonCallback',
+	    contentType: "aplication/json",
+	    dataType: 'json',   
+		//crossDomain : true, 
+	    success: function(json) {
+			graphPoints(json);
+	    },
+	    error: function(e) { 
+	       console.log(e);
+	    },
+	 	onload:function(json){
+			console.log();
+		}
+	});
+	
+}    
