2dc34d573202849b5bcfb5eed6744310482923c6
juvs <juve.guz@gmail.com>
2014-04-21 22:44:57 -0500

    resolviendo

--- src/java/Factories/SensorListFactory.java
+++ src/java/Factories/SensorListFactory.java
@@ -8,7 +8,6 @@ package Factories;
 
 import entities.Sensorlist;
 import java.util.List;
-import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.ejb.Stateless;
