c42162a0d7fcab3a88b3d04aa0d317cefea17d63
mrnesss <mtru92@gmail.com>
2014-05-07 14:56:25 -0500

    servicios actualizado

--- web/cliente/js/request.js
+++ web/cliente/js/request.js
@@ -293,27 +293,3 @@ function requestDescubreSensores() {
         }
     });
 }
-
-function requestServices(query) {
-    var url = 'http://localhost:8080/factoryEcomation_Services/webresources/entities.services/find/' + query;
-    var data = {};
-    $.support.cors = true;
-    $.ajax({
-        type: 'GET',
-        url: url,
-        async: true,
-        jsonpCallback: 'jsonCallback',
-        contentType: "application/json",
-        dataType: 'json',
-        //crossDomain : true,
-        success: function(json) {
-            fillServiceTable(json);
-        },
-        error: function(e) {
-            console.log(e);
-        },
-        onload: function(json) {
-            //console.log(json);
-        }
-    });
-}
